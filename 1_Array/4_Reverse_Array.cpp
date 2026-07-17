e88f3d02eb22d15cc85f0f8418dfd6b990b17600
Prashant Kumar <prashantkumar102369@gmail.com>
2026-07-17 09:18:57 +0530

    reversed array using two pointer approach

---
+++ 1_Array/4_Reverse_Array.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+// reverse array using two pointer approach
+void reverse(int arr[], int size){
+    cout << endl << "Reversing the Array ..." << endl;
+
+    int left = 0;
+    int right = size-1;
+
+    // handled both even and odd size of array
+    while(left < right){
+        int leftEle = arr[left];
+
+        arr[left] = arr[right];
+        arr[right] = leftEle;
+
+        left++; right--;
+
+    }
+
+    cout << "Sucessfully reversed the array" << endl;
+
+}
+
+int main(){
+    int arr[] = {0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+    int size = 16;
+    
+    cout << "Array before reversing: ";
+    for(int i=0; i<size; i++){cout << arr[i] << " ";}
+
+    // reverse array function
+    reverse(arr, size);
+
+    cout << endl <<  "Array after reversing: ";
+    for(int i=0; i<size; i++){cout << arr[i] << " ";}
+
+    
+    cout << endl << "Successful run\n0 code out"; return 0;
+}
